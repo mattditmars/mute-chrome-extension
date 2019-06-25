a7982d789e3e29d03dd3be018020c6f1ad201f9b
Matt Ditmars <ditmarsmatt@gmail.com>
2019-06-25 11:42:32 -0400

    added icons and ability to mute all tabs

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 //event listener for extension button
-chrome.browserAction.onClicked.addListener(function(tab) {
-    muteTab(tab);
+chrome.browserAction.onClicked.addListener(function() {
+    muteAllTabs();
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        let muteState = changeInfo.mutedInfo.muted;
+        muteState ? changeIconMute() : changeIconUnmute();
+    });
 });
 
 //mute currently selected tab
@@ -8,5 +12,32 @@ function muteTab(tab) {
     let isMuted = tab.mutedInfo.muted;
     chrome.tabs.update(tab.id, {
         "muted": !isMuted
+    });
+}
+
+function muteAllTabs() {
+    //query returns array of all tabs that match given property
+    chrome.tabs.query({"audible": true}, function(tabs) {
+        for (const tab of tabs) {
+            muteTab(tab);
+        }
+    })
+}
+
+function changeIconMute() {
+    chrome.browserAction.setIcon({
+        path: {"16": "no-audio-16.png"}
+    });
+    chrome.browserAction.setTitle({
+        "title": "Click to unmute tabs"
+    });
+}
+
+function changeIconUnmute() {
+    chrome.browserAction.setIcon({
+        path: {"16": "speaker-16.png"}
+    });
+    chrome.browserAction.setTitle({
+        "title": "Click to mute tabs"
     });
 }
